7e4d7bffe91f4b406d75973d2a8c3285ba355419
Pavithraa <pavithraapanch@gmail.com>
2022-04-20 16:49:52 -0400

    Added profile functionality

--- src/components/profile/user-list.js
+++ src/components/profile/user-list.js
@@ -1,28 +1,59 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
+import {PublicProfile} from "../profile/public-profile"
+import * as service from "../../services/security-service";
 
 export const UserList = ({users, deleteUser}) => {
-  return (
-    <div className="list-group">
-      {
-        users.map(user => {
-          return (
-            <Link className="list-group-item"
-                  key={user._id}
-                  to={`/home/${user._id}`}>
+    const [currentlyLoggedInUser, setCurrentlyLoggedInUser] = useState({});
+    useEffect(async () => {
+        try {
+            const user = await service.profile();
+            setCurrentlyLoggedInUser(user);
+        } catch (e) {
+            console.log("No User currently logged in!");
+        }
+    }, []);
+
+    return (
+        <div className="list-group">
+            {
+                users.map(user => {
+                    return (
+                        user._id === currentlyLoggedInUser._id ?
+                            <Link className="list-group-item"
+                                  key={user._id}
+                                  to={`/profile/mytuits`}>
           <span className="fs-3">
             {user.username}
           </span>
-              <button onClick={(e) => {
-                e.stopPropagation()
-                e.preventDefault()
-                deleteUser(user._id)
-              }} className="btn btn-danger fa-pull-right">
-                <i className="fas fa-remove"></i>
-              </button>
-            </Link>
-          )
-        })
-      }
-    </div>)
+                                <button onClick={(e) => {
+                                    e.stopPropagation()
+                                    e.preventDefault()
+                                    deleteUser(user._id)
+                                }} className="btn btn-danger fa-pull-right">
+                                    <i className="fas fa-remove"></i>
+                                </button>
+                            </Link>
+
+                            :
+
+                            <Link className="list-group-item"
+                                  key={user._id}
+                                  to={`/public-profile/${user._id}`}>
+          <span className="fs-3">
+            {user.username}
+          </span>
+                                <button onClick={(e) => {
+                                    e.stopPropagation()
+                                    e.preventDefault()
+                                    deleteUser(user._id)
+                                }} className="btn btn-danger fa-pull-right">
+                                    <i className="fas fa-remove"></i>
+                                </button>
+                            </Link>
+
+                    )
+                })
+            }
+        </div>)
 };
--- src/components/tuiter/index.js
+++ src/components/tuiter/index.js
@@ -15,6 +15,7 @@ import More from "../more";
 import {Login} from "../profile/login";
 import Signup from "../profile/signup";
 import TuitScreen from "../tuits/tuit-screen";
+import PublicProfile from "../profile/public-profile";
 
 function Tuiter () {
   return(
@@ -38,6 +39,7 @@ function Tuiter () {
               <Route path="/messages" element={<Messages/>}/>
               <Route path="/bookmarks" element={<Bookmarks/>}/>
               <Route path="/lists" element={<Lists/>}/>
+              <Route path="/public-profile/:uid" element={<PublicProfile/>}/>
               <Route path="/profile/*" element={<Profile/>}/>
               <Route path="/profile/edit" element={<EditProfile/>}/>
               <Route path="/more" element={<More/>}/>
